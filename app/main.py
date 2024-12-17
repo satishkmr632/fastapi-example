5df5a172ad74f9a91d3fc3a1775352548e7f7b34
Satish Kumar <satishkmr632@gmail.com>
2024-12-17 16:14:34 +0530

    Innital comit

--- app/main.py
+++ app/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World staging........."}
+    return {"Hello": "World  new staging........."}
 
 
 @app.get("/items/{item_id}")
